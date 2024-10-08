62e94b9250193c261091a73706e01bc5e2441549
Sunny Soni <149649715+Sunny-Soni00@users.noreply.github.com>
2024-10-08 09:42:41 +0000

    day 6

--- day6/codealong/join.sql
+++ day6/codealong/join.sql
@@ -1,11 +1,57 @@
 -- Joins --
--- 1a. Retrieve the names of players along with the number of games they played in 2020.
--- 1b. Retrieve the names of players along with the names of the parks where their teams played in 2021.
--- 2. Retrieve all player names, along with their performance records, including those players who have no performances in 2021.
--- 3. Retrieve the names of players, their team names, and the salary they earned in 2020.
--- 4. List the names of players who hit more than 20 home runs in a single season, along with the year and the team they played for.
+.mode column
+
+-- 1a. Retrieve the names of players along with the number of games they played in 1920.
+-- Select count(*), players.first_name, players.last_name
+-- From players Join performances
+-- On players.id = performances.player_id
+-- Where year = 1920
+-- Group by performances.player_id;
+
+
+-- 1b. Retrieve the names of players along with the names of the parks where their teams played in 1920.
+
+-- SELECT players.first_name, players.last_name, teams.park
+-- FROM players
+-- JOIN performances ON players.id = performances.player_id
+-- JOIN teams ON performances.team_id = teams.id
+-- WHERE performances.year = 1920;
+
+-- 2. Retrieve all player names, along with their performance records, including those players who have no performances in 1920.
+
+-- Select players.first_name, players.last_name, performances.*
+-- From players
+-- Join performances
+-- On players.id = performances.player_id
+-- Where performances.year = 1920;
+
+-- 3. Retrieve the names of players, their team names, and the salary they earned in 1985.
+-- Select players.first_name, players.last_name, teams.name, salaries.year, salaries.salary
+-- From players
+-- Join salaries
+-- On players.id = salaries.id
+-- JOIN teams
+-- On salaries.team_id = teams.id
+-- Where salaries.year = 1985;
+
+
+-- 4. List the names of players who hit more than 20 home runs in a single season,
+-- along with the year and the team they played for.
+
+-- SELECT players.first_name, players.last_name, performances.year, teams.name, performances.G
+-- FROM players
+-- JOIN performances ON players.id = performances.player_id
+-- JOIN teams ON performances.team_id = teams.id
+-- WHERE performances.G > 20 ;
+
 -- 5. List the first & last name of the top 5 players with most home runs across their career.
 
+Select players.first_name, players.last_name, performances.G
+From players    
+Join performances
+On players.id = performances.player_id
+Order by performances.G DESC
+Limit 5;
 
 
 -- Set Operations --
--- day6/codealong/nested_selection.sql
+++ day6/codealong/nested_selection.sql
@@ -1,7 +1,59 @@
--- Nested Selection --
+-- Nested Selection -- 
+.mode column
+
 -- What if we had to combine information from different tables?
 
 -- 1. Which players are taller than the average height?
--- 2. Which teams have more wins than the league average?
+-- Select AVG(height)
+-- From players;
+
+-- Select first_name, last_name, height
+-- From players
+-- Where height > 71.9
+-- LIMIT 5;
+
+-- 2. Which players were born in the most common birthplace(city)?
+-- Select birth_city, COUNT(birth_city)
+-- from players
+-- Group by birth_city
+-- Order by COUNT(birth_city) DESC
+-- limit 1;
+
 -- 3. Which players have played for the team Atlanta Braves?
+-- SELECT first_name, last_name
+-- FROM players
+-- WHERE id IN (
+-- SELECT player_id
+-- FROM performances
+-- WHERE team_id = 
+-- ( SELECT id
+-- FROM teams
+-- WHERE name = 'Atlanta Braves') );
+
 -- 4. Which players are likely to have played at Olympics Grounds?
+-- SELECT first_name, last_name
+-- FROM players
+-- WHERE id IN (
+-- SELECT player_id
+-- FROM performances
+-- WHERE team_id IN (
+-- SELECT id
+-- FROM teams
+-- WHERE park = 'Olympics Grounds') );
+
+-- 5. Which players have played for the team with the most wins?
+SELECT first_name, last_name
+FROM players
+WHERE id IN (
+SELECT player_id
+FROM performances
+WHERE team_id IN (
+SELECT id
+FROM teams
+WHERE id = (
+SELECT team_id
+FROM (
+SELECT team_id, COUNT(team_id)
+FROM performances
+GROUP BY team_id
+ORDER BY COUNT(team_id) DESC) ) ) );
