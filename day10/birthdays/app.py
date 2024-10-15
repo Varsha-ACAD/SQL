593e1d86b0196f3ecf6b390bc589da545bb12b93
Varsha-ACAD <varsha.v@atriauniversity.edu.in>
2024-10-15 08:31:43 +0000

    Day 10 In Class Changes

--- day10/birthdays/app.py
+++ day10/birthdays/app.py
@@ -7,13 +7,27 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-@app.route("/", methods = ["GET"])
+@app.route("/", methods = ["GET", "POST"])
 def index():
-    return "Welcome to Birthdays!"
+    if request.method == "GET":
+        return "Welcome to Birthdays!"
+    elif request.method == "POST":
+        return jsonify("You cannot POST to Birthdays."), 405
 
 # Before starting, change the visibility of the port 5000 to Public so Postman can access the endpoints!
 
 # 1. Create an API with route /birthdays to display all the birthdays in the database
+@app.route("/birthdays", methods = ["GET", "POST"])
+def birthdays():
+    if request.method == "GET":
+        birthdays = execute_query("SELECT * FROM birthdays")
+        return birthdays, 200
+    elif request.method == "POST":
+        birthday = request.json
+        print(birthday)
+        query = f"INSERT INTO birthdays (name, month, day) VALUES ('{birthday['name']}', {birthday['month']}, {birthday['day']})"
+        execute_query(query)
+        return "Success!"
 
 # 2. Using the same route, allow the user to add a birthday to the database
 
